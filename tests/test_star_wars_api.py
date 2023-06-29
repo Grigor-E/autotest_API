74b737e1d0a2152c9e4145db10997f796606fdfe
Grigor-E <grigor97@gmail.com>
2023-06-29 19:30:57 +0300

    adding 1 people

--- tests/test_star_wars_api.py
+++ tests/test_star_wars_api.py
@@ -43,12 +43,19 @@ def test_star_wars_people():
     print("Метод GET Leia Organa")  # Печать маркера для информированности
     result_get = Swapi_star_wars_api.get_leia_organa()  # Создание переменной со значением вызова метода
     Checking.check_status_code(result_get, 200)
-    token = json.loads(result_get.text)  # Создание переменной для получения текста списка полей
-    print(list(token)) # Печать списка полей
     Checking.check_json_token(result_get, ['name', 'height', 'mass', 'hair_color', 'skin_color', 'eye_color', 'birth_year', 'gender', 'homeworld', 'films', 'species', 'vehicles', 'starships', 'created', 'edited', 'url'])
     Checking.check_json_value(result_get, 'name', 'Leia Organa')
     Checking.check_json_value_text(result_get, 'films')
     Checking.check_json_value(result_get, 'films', ['https://swapi.dev/api/films/1/', 'https://swapi.dev/api/films/2/', 'https://swapi.dev/api/films/3/', 'https://swapi.dev/api/films/6/'])
+
+    print("Метод GET Owen Lars")  # Печать маркера для информированности
+    result_get = Swapi_star_wars_api.get_owen_lars()  # Создание переменной со значением вызова метода
+    Checking.check_status_code(result_get, 200)
+    Checking.check_json_token(result_get, ['name', 'height', 'mass', 'hair_color', 'skin_color', 'eye_color', 'birth_year', 'gender', 'homeworld', 'films', 'species', 'vehicles', 'starships', 'created', 'edited', 'url'])
+    Checking.check_json_value(result_get, 'name', 'Owen Lars')
+    Checking.check_json_value_text(result_get, 'films')
+    Checking.check_json_value(result_get, 'films', ['https://swapi.dev/api/films/1/', 'https://swapi.dev/api/films/5/', 'https://swapi.dev/api/films/6/'])
+
 
 # Получение информации о частях фильма "Звездные войны"
 def test_star_wars_film():
