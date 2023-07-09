fbd28109b192cf13c38fbf7f1d62554bacb182a1
Grigor-E <grigor97@gmail.com>
2023-07-09 13:10:06 +0300

    adding 1 people

--- tests/test_star_wars_api.py
+++ tests/test_star_wars_api.py
@@ -64,6 +64,14 @@ def test_star_wars_people():
     Checking.check_json_value_text(result_get, 'films')
     Checking.check_json_value(result_get, 'films', ['https://swapi.dev/api/films/1/', 'https://swapi.dev/api/films/5/', 'https://swapi.dev/api/films/6/'])
 
+    print("Метод GET R5-D4")  # Печать маркера для информированности
+    result_get = Swapi_star_wars_api.get_r5_d4()  # Создание переменной со значением вызова метода
+    Checking.check_status_code(result_get, 200)
+    Checking.check_json_token(result_get,['name', 'height', 'mass', 'hair_color', 'skin_color', 'eye_color', 'birth_year', 'gender', 'homeworld', 'films', 'species', 'vehicles', 'starships', 'created', 'edited', 'url'])
+    Checking.check_json_value(result_get, 'name', 'R5-D4')
+    Checking.check_json_value_text(result_get, 'films')
+    Checking.check_json_value(result_get, 'films', ['https://swapi.dev/api/films/1/'])
+
 # Получение информации о частях фильма "Звездные войны"
 def test_star_wars_film():
 
